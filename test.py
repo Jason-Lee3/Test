3654293b11bcefd4908b1133a0d1924d0f17cd05
Jason-Lee3 <Jasoneugenelee@gmail.com>
2022-08-18 01:17:58 -0700

    This is second change

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 #this is a test for git
 #Try it 
+
+# this is how to change a file in a github repository
+# $ git status // to check if the file was edited
+# // if it was then you need to add it
+# $ git add <file>
+# // then commit
+# $ git commit -m "YourMesasgeHere"
+# // then push it out
+# $ git push 
